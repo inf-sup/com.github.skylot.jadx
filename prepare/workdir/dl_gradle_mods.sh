33a9ff7b7dd10cd37d36a41fb5d4fc416d60160c
inf-sup <174466778+inf-sup@users.noreply.github.com>
2024-08-15 13:47:41 +0800

    feat: update url

--- prepare/workdir/dl_gradle_mods.sh
+++ prepare/workdir/dl_gradle_mods.sh
@@ -7,7 +7,7 @@ function get_res_info() {
     path="$2"
     url="$3"
     hd "$in" -e '32/1 "%_p"' | grep files | sed -E 's/\.{2,}/\n/g' | grep '^file' > "$path"
-    hd "$in" -e '32/1 "%_p"' | grep files | sed -E 's/\.{2,}/\n/g' | grep '^http' > "$url"
+    hd "$in" -e '32/1 "%_p"' | grep files | sed -E 's/\.{2,}/\n/g' | grep '^http' | xargs -I@ curl -sLI -w '%{url_effective}\n' -o /dev/null @ > "$url"
 }
 
 
@@ -22,12 +22,13 @@ temp_dir=$(mktemp -d)
 res_at_url="$dir/prepare/workdir/resource-at-url.bin"
 file_list="$dir/prepare/workdir/file.list"
 res_path="$dir/res.list"
-res_url="$temp_dir/url.list"
+#res_url="$temp_dir/url.list"
+res_url="$dir/prepare/workdir/url.list"
 
 get_res_info $res_at_url $res_path $res_url
 
 echo -e "url=https://mirrors.huaweicloud.com/openjdk/17.0.2/openjdk-17.0.2_linux-x64_bin.tar.gz\ndigest=" > $file_list
 echo -e "url=https://mirrors.cloud.tencent.com/gradle/gradle-8.7-bin.zip\ndigest=" >> $file_list
-cat $res_url | uniq | xargs -I% echo -e "url=%\ndigest=" >> $file_list
+cat $res_url | sort | uniq | xargs -I% echo -e "url=%\ndigest=" >> $file_list
 
 rm -r "$temp_dir"
