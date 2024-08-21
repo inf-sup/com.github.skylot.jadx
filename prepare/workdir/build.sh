5360e9a0df589e1239b07d30929e0a841dc58fca
inf-sup <174466778+inf-sup@users.noreply.github.com>
2024-08-21 11:22:51 +0800

    feat: update jadx

--- prepare/workdir/build.sh
+++ prepare/workdir/build.sh
@@ -1,4 +1,4 @@
-sleep 1200 && kill -SIGKILL $$ &
+sleep 600 && kill -SIGKILL $$ &
 # jdk 17
 cd /project/linglong/sources
 tar -xf openjdk-17.0.2_linux-x64_bin.tar.gz -C .
@@ -13,9 +13,8 @@ export GRADLE_USER_HOME=/project/linglong/sources/gradle
 
 # build jadx
 cd /project/linglong/sources/jadx.git
-export "JADX_VERSION=1.5.0"
+#export "JADX_VERSION=1.5.0"
 sed -i -E "s#(distributionUrl)=.*#\1=file\\:///project/linglong/sources/gradle-8.10-bin.zip#" gradle/wrapper/gradle-wrapper.properties
-sed -i -E "s#(distributionSha256Sum)=.*#\1=5b9c5eb3f9fc2c94abaea57d90bd78747ca117ddbbf96c859d3741181a12bf2a#" gradle/wrapper/gradle-wrapper.properties
 tar -zxf /project/gradle.tar.gz -C $(dirname $GRADLE_USER_HOME)
 while IFS= read -r line; do
     pre="$GRADLE_USER_HOME/caches/modules-2"
@@ -24,9 +23,6 @@ while IFS= read -r line; do
     mkdir -p $dir
     cp "../$name" "$dir/$name"
 done < "/project/res.list"
-#sed -i -E 's#(org.gradle.parallel)=.*#\1=false#' gradle.properties
-#sed -i '1i org.gradle.daemon=false' gradle.properties
-#sed -i '1i org.gradle.configureondemand=false' gradle.properties
 ./gradlew dist --offline
 cp -r build/jadx/* $PREFIX
 sed -i "s|#!/usr/bin/env sh|#!/usr/bin/env sh\nJAVA_HOME=$jre|" $PREFIX/bin/jadx-gui
@@ -51,5 +47,5 @@ ls $icon_source | grep px.png | sed "s#^.*[^0-9]\([0-9]*\)px[^.]*\.\([^.]*\)\$#$
 ls $icon_source | grep px.png | sed "s#^.*[^0-9]\([0-9]*\)px[^.]*\.\([^.]*\)\$#$icon_source/\0 $icon_target/\1x\1/apps/$icon_name.\2#" | xargs -n 2 cp
 
 # jre
-jd=$($jdeps -q --multi-release 17 --ignore-missing-deps --print-module-deps $JAVA_HOME/jmods build/jadx/lib/jadx-$JADX_VERSION-all.jar)
+jd=$($jdeps -q --multi-release 17 --ignore-missing-deps --print-module-deps $JAVA_HOME/jmods build/jadx/lib/jadx-*-all.jar)
 $jlink --module-path $JAVA_HOME/jmods --add-modules $jd --output $jre
