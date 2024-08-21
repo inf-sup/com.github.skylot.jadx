4aa1ee7669bb83a02bf0a8a2be7b4701c3cc9720
inf-sup <174466778+inf-sup@users.noreply.github.com>
2024-08-21 14:00:52 +0800

    gradle 8.5

--- prepare/workdir/build.sh
+++ prepare/workdir/build.sh
@@ -1,4 +1,4 @@
-sleep 1200 && kill -SIGKILL $$ &
+sleep 600 && kill -SIGKILL $$ &
 # jdk 17
 cd /project/linglong/sources
 tar -xf openjdk-17.0.2_linux-x64_bin.tar.gz -C .
@@ -14,8 +14,8 @@ export GRADLE_USER_HOME=/project/linglong/sources/gradle
 # build jadx
 cd /project/linglong/sources/jadx.git
 export "JADX_VERSION=1.5.0"
-sed -i -E "s#(distributionUrl)=.*#\1=file\\:///project/linglong/sources/gradle-8.10-bin.zip#" gradle/wrapper/gradle-wrapper.properties
-sed -i -E "s#(distributionSha256Sum)=.*#\1=5b9c5eb3f9fc2c94abaea57d90bd78747ca117ddbbf96c859d3741181a12bf2a#" gradle/wrapper/gradle-wrapper.properties
+sed -i -E "s#(distributionUrl)=.*#\1=file\\:///project/linglong/sources/gradle-8.5-bin.zip#" gradle/wrapper/gradle-wrapper.properties
+sed -i -E "s#(distributionSha256Sum)=.*#\1=9d926787066a081739e8200858338b4a69e837c3a821a33aca9db09dd4a41026#" gradle/wrapper/gradle-wrapper.properties
 tar -zxf /project/gradle.tar.gz -C $(dirname $GRADLE_USER_HOME)
 while IFS= read -r line; do
     pre="$GRADLE_USER_HOME/caches/modules-2"
