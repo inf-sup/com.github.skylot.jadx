6369681a531d33e9d2593e10d64e2d8aebb1b569
inf-sup <174466778+inf-sup@users.noreply.github.com>
2024-08-21 10:22:39 +0800

    feat: update gradle
    gradle 8.7 -> 8.10

--- prepare/workdir/build.sh
+++ prepare/workdir/build.sh
@@ -14,7 +14,8 @@ export GRADLE_USER_HOME=/project/linglong/sources/gradle
 # build jadx
 cd /project/linglong/sources/jadx.git
 export "JADX_VERSION=1.5.0"
-sed -i 's#\(distributionUrl\)=\([^=]*\)#\1=file\\:///project/linglong/sources/gradle-8.7-bin.zip#' gradle/wrapper/gradle-wrapper.properties
+sed -i -E "s#(distributionUrl)=.*#\1=file\\:///project/linglong/sources/gradle-8.10-bin.zip#" gradle/wrapper/gradle-wrapper.properties
+sed -i -E "s#(distributionSha256Sum)=.*#\1=5b9c5eb3f9fc2c94abaea57d90bd78747ca117ddbbf96c859d3741181a12bf2a#" gradle/wrapper/gradle-wrapper.properties
 tar -zxf /project/gradle.tar.gz -C $(dirname $GRADLE_USER_HOME)
 while IFS= read -r line; do
     pre="$GRADLE_USER_HOME/caches/modules-2"
@@ -23,10 +24,10 @@ while IFS= read -r line; do
     mkdir -p $dir
     cp "../$name" "$dir/$name"
 done < "/project/res.list"
-sed -i -E 's#(org.gradle.parallel)=[^=]+#\1=false#' gradle.properties
-sed -i '1i org.gradle.daemon=false' gradle.properties
-sed -i '1i org.gradle.configureondemand=false' gradle.properties
-./gradlew dist --max-workers=1 --offline
+#sed -i -E 's#(org.gradle.parallel)=.*#\1=false#' gradle.properties
+#sed -i '1i org.gradle.daemon=false' gradle.properties
+#sed -i '1i org.gradle.configureondemand=false' gradle.properties
+./gradlew dist --offline
 cp -r build/jadx/* $PREFIX
 sed -i "s|#!/usr/bin/env sh|#!/usr/bin/env sh\nJAVA_HOME=$jre|" $PREFIX/bin/jadx-gui
 
